9ef4d4550e03ef886eddd4a4f5c1fc7ed49e5db1
way-19 <asparossystem@gmail.com>
2025-08-14 11:14:02 -0700

    Fix missing JSX closing tag in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -176,6 +176,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
+      </section>
 
         {/* Slide Indicators */}
         <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-3 z-20">
