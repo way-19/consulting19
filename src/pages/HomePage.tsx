49a0c606873d67224071e28819d66e7cfc8cfe9c
way-19 <asparossystem@gmail.com>
2025-08-15 04:24:29 -0700

    Fix Building2 import error in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import {
   Mail, Eye, Sparkles, MessageSquare, Send
 } from 'lucide-react';
 
-import CountryCard from '../components/ModernCountryCard';
+import { ArrowRight, Star, Users, Globe, TrendingUp, CheckCircle, Bot, Zap, Shield, Clock, Award, BarChart3, MessageSquare, Send, Sparkles, Building2 } from 'lucide-react';
 import { useCountries } from '../hooks/useCountries';
 
 const HomePage = () => {
