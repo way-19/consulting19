e1d60fe1140f95e43c4d9a03a91a9bfaf6ef32a6
way-19 <asparossystem@gmail.com>
2025-08-15 02:09:16 -0700

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -143,7 +143,7 @@ const HomePage = () => {
             </div>
           ) : (
             <>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                 {featuredCountries.map((country) => (
                   <ModernCountryCard key={country.id} country={country} />
                 ))}
