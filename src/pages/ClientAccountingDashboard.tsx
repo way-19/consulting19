38262de3b73ebea2818aebb394982107367bb79f
way-19 <asparossystem@gmail.com>
2025-08-15 10:55:22 -0700

    Fix JSX multiple elements error

--- src/pages/ClientAccountingDashboard.tsx
+++ src/pages/ClientAccountingDashboard.tsx
@@ -445,7 +445,8 @@ const ClientAccountingDashboard = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <>
+      <div className="min-h-screen bg-gray-50">
       {/* Header */}
       <div className="bg-white shadow-sm border-b border-gray-200">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
@@ -1034,6 +1035,7 @@ const ClientAccountingDashboard = () => {
           </div>
         </div>
       </div>
+      </div>
 
       {/* Invoice Payment Checkout */}
       {showInvoiceCheckout && selectedInvoice && (
@@ -1156,7 +1158,7 @@ const ClientAccountingDashboard = () => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
